cb92388ad8bb94e8cd5aabc4534d0470362d1759
EfoJensen <Jensena231@gmail.com>
2025-03-15 12:19:37 +0000

    Ensured DB is connected to api on startup

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,24 @@
 import express from "express";
+import mongoose from "mongoose";
 import jobRoutes from './routes/jobs';
 import userRoutes from './routes/users';
 
 const PORT = 3000;
 const app = express();
 
+async function connectToDB () {
+    try {
+        await mongoose.connect(process.env["DATABASE_URL"]!);
+        console.log('Connection to Db successful')
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 app.use('/jobs', jobRoutes);
 app.use('/users', userRoutes);
 
 app.listen(PORT, () => {
+    connectToDB();
     console.log("Server running on http://localhost:" + PORT);
 })
